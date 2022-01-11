938d88b73d8a3b3d3f39b8c682ce675a3d616884
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-11 18:38:32 +0330

    initializing files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
+import React from 'react'
 
 function App() {
-  return (
-    <div >
-      hello world
-    </div>
-  );
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
