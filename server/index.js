480606e617bc7fdf495c96954e2d7619954bfd73
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-17 13:49:33 +0330

    routes

--- server/index.js
+++ server/index.js
@@ -15,13 +15,9 @@ app.use(cors())
 const CONNECTION_URL  = 'mongodb://benyamin:591192@benyamincluster-shard-00-00.9wfsm.mongodb.net:27017,benyamincluster-shard-00-01.9wfsm.mongodb.net:27017,benyamincluster-shard-00-02.9wfsm.mongodb.net:27017/memorize?ssl=true&replicaSet=atlas-lju0ji-shard-0&authSource=admin&retryWrites=true&w=majority'
 const PORT  = process.env.PORT || 5000 ;
 
-mongoose.connect(CONNECTION_URL , {
-    useNewUrlParser : true,
-    useUnifiedTopology: true
-}).then(() =>{
+mongoose.connect(CONNECTION_URL).then(() =>{
     app.listen(PORT , ()=> console.log(`server running on port ${PORT}`))
 }).catch((error) => console.log(error.message))
 
-mongoose.set('useFindAndModify' , false)
 
 
