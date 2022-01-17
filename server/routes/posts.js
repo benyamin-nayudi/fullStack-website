17fcbc042fe8f1b89aa07799fb3b53bcefd2d8a0
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-17 13:49:02 +0330

    setting routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,13 @@
 import express from "express";
-
+import { getPosts } from "../controllers/posts.js";
 
 
 const router = express.Router()
 
+router.get('/' , getPosts)
 
 
 
-
-
+export default router
 
 
