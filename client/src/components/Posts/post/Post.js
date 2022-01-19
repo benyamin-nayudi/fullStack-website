7ede9b4a6eb772f5de9b57e51c7f13676a6423fb
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-19 08:59:38 +0330

    update

--- client/src/components/Posts/post/Post.js
+++ client/src/components/Posts/post/Post.js
@@ -13,7 +13,8 @@ const Post = ({post}) =>{
 
     return (
         <Card className = {mergeClasses.Card}>
-            <CardMedia className={classes.media} image = {post.selectedFile} title={post.title}/>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+
             <div className={classes.overlay}>
                 <Typography variant="h6" > {post.creator}</Typography>
                 <Typography variant="body2" > {moment(post.createdAt).fromNow()}</Typography>
